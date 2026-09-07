d1688a1c1f188fec210bf29c34402408e62058ef
Sristi Chowdhury <53037053+Sristi27@users.noreply.github.com>
2026-09-07 07:21:50 +0000

    Delete retailorderhub directory

--- retailorderhub/src/main/java/com/training/retailorderhub/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.training.retailorderhub.repository;
-
-import com.training.retailorderhub.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- retailorderhub/src/main/java/com/training/retailorderhub/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.training.retailorderhub.repository;
-
-import com.training.retailorderhub.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
